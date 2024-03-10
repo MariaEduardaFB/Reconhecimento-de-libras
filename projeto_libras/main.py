ac857381475133b384d253b6e85db4bd0f004fc2
Guilherme Bandeira <107276182+Asphyan@users.noreply.github.com>
2024-03-10 18:00:52 -0300

    Update main.py

--- projeto_libras/main.py
+++ projeto_libras/main.py
@@ -1,19 +1,19 @@
 import cv2
 import mediapipe as mp
-import keras.models import load_model
+from keras.models import load_model
 import numpy as np
 
 cap = cv2.VideoCapture(0)
 
 hands = mp.solutions.hands.Hands(max_num_hands=1)
 
-classes = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+classes = ['A','B','C','D']
 model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 while True:
-    sucess, img = cap.read()
-    frameRGB = cv2.cvtcolor(img,cv2.COLOR_BGR2RGB)
+    success, img = cap.read()
+    frameRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     results = hands.process(frameRGB)
     handsPoints = results.multi_hand_landmarks
     h, w, _ = img.shape
@@ -34,22 +34,21 @@ while True:
                     y_max = y
                 if y < y_min:
                     y_min = y
-
-            cv2.rectangle(img, (x_min-50, y_min-50), (x_max+50, y_max+50), (0, 55, 0), 2)
+            cv2.rectangle(img, (x_min-50, y_min-50), (x_max+50, y_max+50), (0, 255, 0), 2)
 
             try:
-                imgCrop = img[y_min-50:y_max+50, x_min-50, x_max+50]
+                imgCrop = img[y_min-50:y_max+50,x_min-50:x_max+50]
                 imgCrop = cv2.resize(imgCrop,(224,224))
                 imgArray = np.asarray(imgCrop)
                 normalized_image_array = (imgArray.astype(np.float32) / 127.0) - 1
                 data[0] = normalized_image_array
                 prediction = model.predict(data)
                 indexVal = np.argmax(prediction)
-
-                cv2.putText(img, classes[indexVal],(x_min-50,y_min-65),cv2.FRONT_HERSHEY_COMPLEX,3,(0,00,255),5)
+                #print(classes[indexVal])
+                cv2.putText(img,classes[indexVal],(x_min-50,y_min-65),cv2.FONT_HERSHEY_COMPLEX,3,(0,0,255),5)
 
             except:
                 continue
 
-cv2.imshow('Imagem',img)
-cv2.waitKey(1)
+    cv2.imshow('Imagem',img)
+    cv2.waitKey(1)
